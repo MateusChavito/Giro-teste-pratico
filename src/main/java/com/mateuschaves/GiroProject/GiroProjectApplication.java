cab870acc433e270f97210a083d1cbe6ead1b957
MateusChavito <mateus.chavito@hotmail.com>
2025-02-21 13:22:37 -0300

    Configuração inicial do projeto: Estrutura e dependências

---
+++ src/main/java/com/mateuschaves/GiroProject/GiroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mateuschaves.GiroProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiroProjectApplication.class, args);
+	}
+
+}
