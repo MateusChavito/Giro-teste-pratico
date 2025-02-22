786a2425fc6d8c3a087090996e1b1ad3f92bfa8b
MateusChavito <mateus.chavito@hotmail.com>
2025-02-22 12:08:37 -0300

    feat(refactor): Criar e mover lógica do CurrencyController para o CurrencyService.

--- src/main/java/com/mateuschaves/GiroProject/controller/CurrencyController.java
+++ src/main/java/com/mateuschaves/GiroProject/controller/CurrencyController.java
@@ -2,6 +2,7 @@ package com.mateuschaves.GiroProject.controller;
 
 import com.mateuschaves.GiroProject.model.Currency;
 import com.mateuschaves.GiroProject.repository.CurrencyRepository;
+import com.mateuschaves.GiroProject.service.CurrencyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,47 +11,43 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/currencies")
+@RequestMapping("/api/currencies")
 public class CurrencyController {
 
-    private final CurrencyRepository currencyRepository;
+    private final CurrencyService currencyService;
 
-    public CurrencyController(CurrencyRepository currencyRepository) {
-        this.currencyRepository = currencyRepository;
+    public CurrencyController(CurrencyService currencyService){
+        this.currencyService = currencyService;
     }
 
     @GetMapping
-    public List<Currency>getAllCurrencies(){
-        return currencyRepository.findAll();
+    public ResponseEntity<List<Currency>> getAllCurrencies() {
+        List<Currency> currencies = currencyService.getAllCurrencies();
+        return new ResponseEntity<>(currencies, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Currency> getCurrencyById(@PathVariable Long id){
+        Currency currency = currencyService.getCurrencyById(id);
+        return  new ResponseEntity<>(currency, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Currency> createCurrency(@RequestBody Currency currency) {
-        Currency savedCurrency = currencyRepository.save(currency);
+        Currency savedCurrency = currencyService.createCurrency(currency);
         return new ResponseEntity<>(savedCurrency, HttpStatus.CREATED);
     }
 
-    @PutMapping
-    public ResponseEntity<Currency> updateCurrency(@PathVariable Long id, @RequestBody Currency currencyDetails){
-        Optional<Currency> currencyOptional = currencyRepository.findById(id);
-        if(currencyOptional.isPresent()){
-            Currency currency = currencyOptional.get();
-            currency.setName(currencyDetails.getName());
-            currency.setType(currencyDetails.getType());
-            currencyRepository.save(currency);
-            return new ResponseEntity<>(currency, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @PutMapping("/{id}")
+    public ResponseEntity<Currency> updateCurrency(@PathVariable Long id, @RequestBody Currency currencyDetails) {
+        Currency updatedCurrency = currencyService.updateCurrency(id, currencyDetails);
+        return  new ResponseEntity<>(updatedCurrency, HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Currency> deleteCurrency(@PathVariable Long id){
-        if(currencyRepository.existsById(id)){
-            currencyRepository.existsById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Currency> deleteCurrency(@PathVariable Long id) {
+        currencyService.deleteCurrency(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-
+    
 }
---
+++ src/main/java/com/mateuschaves/GiroProject/service/CurrencyService.java
@@ -0,0 +1,48 @@
+package com.mateuschaves.GiroProject.service;
+
+import com.mateuschaves.GiroProject.model.Currency;
+import com.mateuschaves.GiroProject.repository.CurrencyRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CurrencyService {
+
+    private final CurrencyRepository currencyRepository;
+
+    public CurrencyService(CurrencyRepository currencyRepository){
+        this.currencyRepository = currencyRepository;
+    }
+
+    public List<Currency> getAllCurrencies(){
+        return currencyRepository.findAll();
+    }
+
+    public Currency createCurrency(Currency currency){
+        return currencyRepository.save(currency);
+    }
+
+    public Currency getCurrencyById(Long id){
+        return currencyRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Moeda não foi encontrada."));
+    }
+
+    public Currency updateCurrency(Long id, Currency currencyDetails){
+        Currency currency = getCurrencyById(id);
+        currency.setName(currencyDetails.getName());
+        currency.setType(currencyDetails.getType());
+        return currencyRepository.save(currency);
+    }
+
+    public void deleteCurrency(Long id){
+        Currency currency = getCurrencyById(id);
+        currencyRepository.delete(currency);
+    }
+
+
+}
